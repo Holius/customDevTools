c244cf22193c0fc52888a0f7cee1fac8d784e6f9
Justin Gilley <justindgilley@gmail.com>
2021-01-23 13:45:24 -0600

    add element regex helper

--- console.js
+++ console.js
@@ -13,6 +13,27 @@
             window.a.sa = (selector, elm) => {
                 return elm.querySelectorAll(selector)
             } 
+            //returns (if extant) first regex match for DOM element in sibling then children 
+            HTMLElement.prototype.contains_sc = function (regex, direction = 0) { //do NOT use lambda since this cannot go one level higher
+                const elm = this //readability
+                if (!elm) return
+                if (elm.textContent.match(regex)) return elm.textContent.match(regex)[0]
+                
+                if (elm.previousElementSibling && direction <= 0) {
+                    const result = elm.contains_sc.call(elm.previousElementSibling, regex, -1)
+                    if (result) return result
+                }
+                if (elm.nextElementSibling && direction >= 0) { 
+                    const result = elm.contains_sc.call(elm.previousElementSibling, regex, 1)
+                    if (result) return result
+                } 
+            
+                for (let i = 0; i < elm.children.length; i++) {
+                    const child = elm.children[i]
+                    const result = elm.contains_sc.call(child, regex, null)
+                    if (result) return result
+                } 
+            }
             console.log('window.a added to state')
         } 
     }
